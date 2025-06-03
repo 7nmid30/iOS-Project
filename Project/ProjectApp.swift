a9d8264fabbf1ab5bf42ae8e84dc53e81b31b4c4
Satoshi <allgoodday10@gmail.com>
2025-06-03 15:55:32 +0900

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by 高見聡 on 2025/01/12.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
